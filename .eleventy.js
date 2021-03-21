3364a7b233fb09c1792756bdac3a9d9bbf785812
Shawn Thompson <plansmash@me.com>
2021-03-21 10:06:03 -0400

    removed 11ty sass plugin

--- .eleventy.js
+++ .eleventy.js
@@ -11,11 +11,6 @@ module.exports = function(config) {
     return coll
   });
 
-  const pluginSass = require("eleventy-plugin-sass");
-	module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
-	};
-
 	return {
 		dir: {
 			input : "./src/pages",
