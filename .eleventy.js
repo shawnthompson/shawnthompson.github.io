723f5254de385e4e8e8525fb80ba6be1976b36c2
Shawn Thompson <plansmash@me.com>
2021-03-21 09:52:33 -0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+
+  config.addCollection("posts", function(collection) {
+    const coll = collection.getFilteredByTag("post")
+    for(let i = 0; i < coll.length; i++) {
+      const prevPost = coll[i-1]
+      const nextPost = coll[i+1]
+      coll[i].data["prevPost"] = prevPost
+      coll[i].data["nextPost"] = nextPost
+    }
+    return coll
+  });
+
+  const pluginSass = require("eleventy-plugin-sass");
+	module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
+	};
+
+	return {
+		dir: {
+			input : "./src/pages",
+			output : "dist",
+			includes : "../_includes",
+			data : "../_data"
+		},
+		templateFormats : ["html", "md", "njk", "css"],
+		htmlTemplateEngine : "njk",
+		markdownTemplate : "njk"
+	};
+}
---
+++ src/posts/my-first-post.md
@@ -0,0 +1,10 @@
+---
+title: "My first post"
+layout: ../layouts/base.njk
+date: "2021-03-19"
+tags: post
+---
+
+## This is a heading level 2
+ 
+This is a paragraph in Markdown
