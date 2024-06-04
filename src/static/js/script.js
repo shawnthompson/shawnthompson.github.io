3c5bca0e912f877b9616b21d8c2e5b26abcea36f
Shawn Thompson <plansmash@me.com>
2024-06-03 21:52:27 -0400

    Remove summary error message

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -38,7 +38,7 @@ function validateField({ elField, validateFn }) {
 		validateFn(e.target, { removeOnly: true });
 
 		if (hasSubmitted) {
-			updateSubmitSummary();
+			validateAllFields();
 		}
 	});
 
@@ -88,39 +88,10 @@ function updateFieldDOM(el, isValid, errorMessage, opts) {
 	}
 }
 
-function updateSubmitSummary({ isSubmit } = {}) {
-	const elSummary = elForm.querySelector(".form-feedback");
-	const elSummaryMsg = elSummary.querySelector(".form-feedback-msg");
-
-	// Clear form feedback
-	elSummaryMsg.classList.remove("isInvalid");
-	elSummaryMsg.classList.remove("isSuccess");
-	elSummaryMsg.innerText = "";
-	const errorsState = Object.values(formErrors);
-
-	if (errorsState.includes(true)) {
-		// Show error msg
-		const errorCount = Object.values(formErrors).reduce((total, hasError) => {
-			return hasError ? total + 1 : total;
-		}, 0);
-		const errorMsg =
-			errorCount === 1
-				? `Failed to save because ${errorCount} field is invalid.`
-				: `Failed to save because ${errorCount} fields are invalid.`;
-		elSummaryMsg.classList.add("isInvalid");
-		elSummaryMsg.innerText = errorMsg;
-
-		elSummary.querySelector(".form-feedback-sr").innerText = isSubmit
-			? // Set SR error message only on submit to avoid being re-announced
-			// every time the error summary visually changes.
-			errorMsg
-			: "";
-	} else if (isSubmit) {
-		const successMsg = "Saved with success.";
-		elSummary.querySelector(".form-feedback-sr").innerText = successMsg;
-		elSummaryMsg.innerText = successMsg;
-		elSummaryMsg.classList.add("isSuccess");
-	}
+function validateAllFields() {
+	validateFieldEmail(elEmail);
+	validateFieldName(elName);
+	validateFieldMessage(elMessage);
 }
 
 elForm.addEventListener("submit", (e) => {
@@ -128,9 +99,7 @@ elForm.addEventListener("submit", (e) => {
 	hasSubmitted = true;
 
 	// Validate all fields
-	validateFieldEmail(elEmail);
-	validateFieldName(elName);
-	validateFieldMessage(elMessage);
+	validateAllFields();
 
 	// Check if all form errors are false
 	const allFieldsValid = Object.values(formErrors).every(error => !error);
@@ -147,8 +116,5 @@ elForm.addEventListener("submit", (e) => {
 		} else if (formErrors.message) {
 			elMessage.focus();
 		}
-
-		// Update the submit summary with error message
-		updateSubmitSummary({ isSubmit: true });
 	}
 });
